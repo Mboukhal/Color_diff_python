e6f9d0c774ccb27de1c799bc08d1740d56fbe953
amin <lios80466@gmail.com>
2022-07-24 15:39:37 +0100

    Sun Jul 24 15:39:37 +01 2022

--- dfii.py
+++ dfii.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-		Color diff 0.1
+    py_diff 0.1
+    made by kai/Mboukhal
 """
 
 GREEN = '\033[0;32m'
@@ -12,7 +13,6 @@ def my_split(s):
     x = []
     i = 0
     slen = len(s)
-    # print (slen)
     while i < slen:
         x.append(s[i])
         i += 1
@@ -27,6 +27,11 @@ def rev_str(s):
     ns += s[slen]
     return ns
 
+def get_last(s, end):
+    elen = len(end)
+
+    print ('|'+s[:-elen]+'|', end)
+
 def check_str(s1, s2):
     i = 0
     x1 = my_split(s1)
@@ -35,22 +40,19 @@ def check_str(s1, s2):
     len2 = len(x2)
     str_res_start = ''
     str_res_end = ''
-    while  len1 > i and len2 > i:
-        if x1[i] == x2[i]:
-            str_res_start += x1[i]
+    while x1[i] == x2[i]:
+        str_res_start += x1[i]
         i += 1
     len1 -= 1
     len2 -= 1
     while  x1[len1] == x2[len2]:
         str_res_end += x1[len1]
-        # print (x1[len1], len1, len2)
         len1 -= 1
         len2 -= 1
-    # print (s1, s2)
     str_res_end = rev_str(str_res_end)
-    # print ()
-    # print ("start", str_res_start, str_res_end)
-    return str_res_start, str_res_end
+    slen = len(str_res_start) 
+    elen = len(str_res_end)
+    return str_res_start, str_res_end, s1[slen:-elen], s2[slen:-elen]
 
 def color_diff(s1, s2):
     i = 0
@@ -67,16 +69,8 @@ def color_diff(s1, s2):
         if x1[i] == x2[i]:
             str_res += x1[i] + ' '
         else:
-            # x1[i], x2[i] = 
-            n_start, n_end = check_str(x1[i], x2[i])
-            # while x1[i][i1] == x2[i][i1]:
-            #     start += x1[i][i1]
-            #     i1 += 1
-            # i2 = len(x1[i]) - 1
-            # while x1[i][i2] == x2[i][i2]:
-            #     end += x1[i][i2]
-            #     i2 -= 1
-            str_res += f'[{RED}{x1[i]}{NC}/{GREEN}{x2[i]}{NC}]' + ' '
+            n_start, n_end, ss1, ss2 = check_str(x1[i], x2[i])
+            str_res += f'{n_start}[{RED}{ss1}{NC}/{GREEN}{ss2}{NC}]{n_end}' + ' '
         
         i += 1
     print(str_res)
@@ -97,7 +91,6 @@ token (|\\\\\$USER|, 4)
 token (|/Users/piola/Desktop/parsing 2|, 8)
 cmd(3, [echo -nnnsdkfghsdkfjghnnnnn -77nnnkljptrtfnnn88 hello world;             echo '\-\n' '\h\e\l\l\o'; echo \\\\\\\\\\\$USER  $PWD])
 '''
-# print (s1)
 s2 =  '''
 token (|echo|, 0)
 token (|-nsdkfghj|, 9)
@@ -114,10 +107,10 @@ token (|\\\\\$USER|, 4)
 token (|/Users/piola/lilo/parsing 2|, 8)
 cmd(3, [echo -nnnsdkfghsdkfjghnnnnn -77nnnkljptrtfnnn88 hello world;             echo '\-\n' '\h\e\l\l\o'; echo \\\\\\\\\\\$USER  $PWD])
 '''
-r1 = 'hi lilo ok/Users/piola/lilo/parsing'
-r2 = 'hi kaio ok/Users/piola/lilo/parsing'
+r1 = 'hi slilo ok/Users/piola/lilo/parsing'
+r2 = 'hi skaio ok/Users/piola/lilo/parsing'
 
-# color_diff(r1, r2)
-# print()
-# print()
+color_diff(r1, r2)
+print()
+print()
 color_diff(s1, s2)
